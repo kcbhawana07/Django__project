824c7c993c92b0c549d958e2508f17a11ee4d8ef
kcbhawana07 <kcbhawana78@gmail.com>
2026-02-14 20:51:15 +0545

    add service and service category models

--- ecommerce_project/services/models.py
+++ ecommerce_project/services/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField(help_text="Duration in minutes")
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
