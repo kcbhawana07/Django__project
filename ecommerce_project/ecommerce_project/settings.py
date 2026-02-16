a11177c725aec96f730fae1f3e262293057ca1f8
kcbhawana07 <kcbhawana78@gmail.com>
2026-02-16 08:34:32 +0545

    feat(accounts): configure authentication and registration templates

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'ecommerce_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],  # Add this line for rendering template
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
