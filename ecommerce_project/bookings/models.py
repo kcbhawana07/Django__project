f3dbbebeacfa376543f67caab15010dc2c9ee928
kcbhawana07 <kcbhawana78@gmail.com>
2026-02-16 10:48:44 +0545

    feat(bookings): add booking model with unique time-slot constraint

--- ecommerce_project/bookings/models.py
+++ ecommerce_project/bookings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from services.models import Service
 # Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    booking_date = models.DateField()
+    time_slot = models.TimeField()
+
+    status = models.CharField(max_length=20, default='Confirmed')
+
+    class Meta:
+        unique_together = ('service', 'booking_date', 'time_slot')
+
+    def __str__(self):
+        return self.service.name
