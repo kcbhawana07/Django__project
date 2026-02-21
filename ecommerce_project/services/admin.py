bb36bad093a3dae51921bb38c40e2ab107656138
kcbhawana07 <kcbhawana78@gmail.com>
2026-02-21 15:42:53 +0545

    fix(services): remove duplicate Service model registration in admin

--- ecommerce_project/services/admin.py
+++ ecommerce_project/services/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import Service, ServiceCategory
 # Register your models here.
 
-admin.site.register(ServiceCategory)
-admin.site.register(Service)
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
